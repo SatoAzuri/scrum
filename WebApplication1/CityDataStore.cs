9841ccc1c44740b61649da2b32a248d70fc5e094
SatoAzury <13bony13@gmail.com>
2017-10-28 13:18:15 -0400

    Commit Staged

---
+++ WebApplication1/CityDataStore.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id=1,
+                    Name="Nastya",
+                    Description="Awesome, Hot, Fun, Nice",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=1,
+                            Name = "Developer",
+                            Description="Coding, front, back"
+                        },
+                         new PointOfInterestDto()
+                        {
+                            Id=2,
+                            Name = "Mom",
+                            Description="Kids, !Cook, Zumba"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id=2,
+                    Name="Pavan",
+                    Description="Kind, Funny, Zen",
+                     PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=3,
+                            Name = "Mentor",
+                            Description="Help, Coding, Supervise"
+                        },
+                         new PointOfInterestDto()
+                        {
+                            Id=4,
+                            Name = "Man",
+                            Description="Girls, Drinks, Sport"
+                        }
+                    }
+                },
+                  new CityDto()
+                {
+                    Id=3,
+                    Name="Gireesh",
+                    Description="Smart, Fun, Leader",
+                     PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id=5,
+                            Name = "Leader",
+                            Description="Motivated, Detail orientated, great commuticational skills"
+                        }
+                      
+                    }
+                }
+
+            };
+
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController: Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult GetPointsOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == id);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+        //last point!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      /*  [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult CreatePointsOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreation pointOfInterest)
+        {
+
+        }*/
+    }
+}
